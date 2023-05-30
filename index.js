f1fff9da90b74e4d63d075626b9f3e7990aaca81
Abdielsantillan <santillanabdiel0@gmail.com>
2023-05-30 17:41:39 -0600

    add functions for the grid Function implementation, the first function draw a * when there is a 1 in the array and a "." when there is a 0 the update function is to update the array and change the 0´s and 1´s depending on the given conditions Co-authored-by: Eduardoespl <eduardoesp3317@gmail.com>

--- index.js
+++ index.js
@@ -1,14 +1,75 @@
 var x = 4;
 var y = 8;
 
-var matriz = new Array(x);
+let Cuadricula = [
+    [0,0,0,1,0,0,0,0],
+    [0,0,0,1,0,0,0,0],
+    [0,0,1,1,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+];
 
-for (var i = 0; i<x; i++){
-    matriz[i] = new Array(y);
-    for (var j = 0; j < y; j++){
-        matriz[i][j] = '.'
-        console.log(matriz[i][j])
+function drawCell(){
+    console.clear();
+    for(let i=0; i< Cuadricula.length; i++){
+        let row = "";
+        for(let j=0; j<Cuadricula[i].length; j++){
+            if(Cuadricula[i][j]===0){
+                row += ".";
+            }else{
+                row += "*";
+            }
+        }
+        console-console.log(row);
     }
 }
 
-/* console.log(matriz); */
+
+function update(){
+    let newTable = Cuadricula;
+    for(let i =0; i<x; i++){
+        for(let j = 0; j<y; j++){
+            const neighbors = countNeighbors(i,j);
+            if (Cuadricula[i][j]==1){
+                if(neighbors ===2 || neighbors === 3){
+                    newTable[i][j]=1;
+                }
+            }else{
+                if(neighbors ===3){
+                    newTable[i][j]=1;
+                }
+            }
+        }
+    }
+    Cuadricula = newTable;
+}
+
+function countNeighbors(row, col){
+    let count = 0;
+    for ( let i = -1;i <= 1; i++){
+        for (let j = -1; j<= 1; j++){
+            if( i=== 0 && j == 0)continue;
+            const newRow = row + i;
+            const newCol = col + j;
+            if(
+                newRow >= 0 &&
+                newRow < x &&
+                newCol >= 0 &&
+                newCol < y &&
+                Cuadricula[newRow][newCol] ===1
+            ){
+                count++;
+            }
+            
+        }
+    }
+    return count;
+}
+
+function starGame(){
+    setInterval(()=>{
+        update();
+        drawCell();
+    }, 2000);
+}
+
+starGame();
