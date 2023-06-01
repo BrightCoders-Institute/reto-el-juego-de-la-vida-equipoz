bc1f0a1856fa36eb94afb3bb92606324802f635b
Eduardoespl <eduardoesp3317@gmail.com>
2023-05-31 18:51:14 -0600

    Refactored code to object-oriented approach
    Created the "GameOfLife" class with a constructor inside that get the values from "numRows", "numCols" and "numGenerations" and set them as properties of the class.
    Added run method to the class that will run the game for the number of generations specified by the user.
    
    Co-authored-by: J3fte <santillanabdiel0@gmail.com>

--- index.js
+++ index.js
@@ -1,66 +1,80 @@
+class GameOfLife {
+  constructor (numRows, numCols, numGenerations) {
+    this.numRows = numRows
+    this.numCols = numCols
+    this.numGenerations = numGenerations
+    this.grid = this.createGrid()
+  }
+
+  createGrid () {
+    const grid = []
+
+    const pattern = [
+      ['*', '.', '.', '.', '.', '.', '*', '.'],
+      ['.', '*', '*', '.', '*', '.', '.', '*'],
+      ['.', '.', '.', '*', '*', '.', '*', '.'],
+      ['.', '*', '.', '.', '.', '.', '.', '.']
+    ]
+
+    for (let row = 0; row < this.numRows; row++) {
+      const rowArray = []
+      for (let col = 0; col < this.numCols; col++) {
+        rowArray.push(pattern[row][col])
+      }
+      grid.push(rowArray)
+    }
+
+    return grid
+  }
+
+  nextGeneration () {
+    const newGrid = [...this.grid.map(row => [...row])]
+
+    for (let row = 0; row < this.numRows; row++) {
+      for (let col = 0; col < this.numCols; col++) {
+        let aliveNeighbors = 0
+
+        for (let i = -1; i <= 1; i++) {
+          for (let j = -1; j <= 1; j++) {
+            if (i === 0 && j === 0) continue
+
+            const neighborRow = row + i
+            const neighborCol = col + j
+
+            if (
+              neighborRow >= 0 &&
+              neighborRow < this.numRows &&
+              neighborCol >= 0 &&
+              neighborCol < this.numCols &&
+              this.grid[neighborRow][neighborCol] === '*'
+            ) {
+              aliveNeighbors++
+            }
+          }
+        }
+        if (this.grid[row][col] === '*' && (aliveNeighbors < 2 || aliveNeighbors > 3)) {
+          newGrid[row][col] = '.'
+        } else if (this.grid[row][col] === '.' && aliveNeighbors === 3) {
+          newGrid[row][col] = '*'
+        }
+      }
+    } this.grid = newGrid
+  }
+
+  run () {
+    console.log('Generacion 1: ')
+    console.log(this.grid.map(row => row.join('')).join('\n'))
+    for (let generation = 2; generation <= this.numGenerations; generation++) {
+      this.nextGeneration()
+      console.log(`\nGeneración ${generation}:`)
+      console.log(this.grid.map(row => row.join('')).join('\n'))
+    }
+  }
+}
+
 const numRows = 4
 const numCols = 8
 const numGenerations = 8
 
-function createGrid () {
-  const grid = []
-
-  const pattern = [
-    ['*', '.', '.', '.', '.', '.', '*', '.'],
-    ['.', '*', '*', '.', '*', '.', '.', '*'],
-    ['.', '.', '.', '*', '*', '.', '*', '.'],
-    ['.', '*', '.', '.', '.', '.', '.', '.']
-  ]
-
-  for (let row = 0; row < numRows; row++) {
-    const rowArray = []
-    for (let col = 0; col < numCols; col++) {
-      rowArray.push(pattern[row][col])
-    }
-    grid.push(rowArray)
-  }
-
-  return grid
-}
-
-function nextGeneration (grid) {
-  const numRows = grid.lrngth
-  const numCols = grid[0].lenght
-  const newGrid = [...grid.map(row => [...row])]
-  for (let row = 0; row < numRows; row++) {
-    for (let col = 0; col < numCols; col++) {
-      let aliveNeighbors = 0
-      for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-          if (i === 0 && j === 0) continue
-          const neighborRow = row + i
-          const neighborCol = col + j
-          if (
-            neighborRow >= 0 &&
-            neighborRow < numRows &&
-            neighborCol >= 0 &&
-            neighborCol < numCols &&
-            grid[neighborRow][neighborCol] === '*'
-          ) {
-            aliveNeighbors++
-          }
-        }
-      }
-      if (grid[row][col] === '*' && (aliveNeighbors < 2 || aliveNeighbors > 3)) {
-        newGrid[row][col] = '.'
-      } else if (grid[row][col] === '.' && aliveNeighbors === 3) {
-        newGrid[row][col] = '*'
-      }
-    }
-  } return newGrid
-}
-
-let grid = createGrid()
-console.log('Generacion 1: ')
-console.log(grid.map(row => row.join('')).join('\n'))
-
-for (let generation = 2; generation <= numGenerations; generation++) {
-  grid = nextGeneration(grid)
-  console.log(`\nGeneración ${generation}:`)
-  console.log(grid.map(row => row.join('')).join('\n'))
-}
+const game = new GameOfLife(numRows, numCols, numGenerations)
+game.run()
